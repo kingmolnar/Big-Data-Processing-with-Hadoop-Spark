7fe5da98f4546d71d91ceb6fdcdfc822f5b9ae29
kingmolnar <peter@kingmolnar.com>
2017-01-16 15:58:59 -0500

    update

--- 01_MapReduce_HDFS/WordCount/runmr.sh
+++ 01_MapReduce_HDFS/WordCount/runmr.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+WD=`pwd`
 
 yarn jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
 	-input $1 \
-	-output /home/pmolnar/streamout \
-	-mapper ./mapper.py \
-	-reducer ./reducer.py
+	-output /home/$USER/streamout \
+	-mapper $WD/mapper.py \
+	-reducer $WD/reducer.py
 
